f3245f7c880cdfa2297a3870abfc6ef28ec8d681
kursor <skurochkin298@gmail.com>
2022-08-16 04:11:22 +0300

    settings change, ConnectionModule.kt and module domain

--- app/src/main/java/com/kursor/chroniclesofww2/Settings.kt
+++ app/src/main/java/com/kursor/chroniclesofww2/Settings.kt
@@ -1,15 +1,14 @@
 package com.kursor.chroniclesofww2
 
 import com.kursor.chroniclesofww2.data.repositories.settingsRepositories.SettingsRepository
-import com.kursor.chroniclesofww2.data.repositories.settingsRepositories.SharedPrefSettingsRepository
+import com.kursor.chroniclesofww2.features.ChangePasswordReceiveDTO
 import com.kursor.chroniclesofww2.objects.Const
 import io.ktor.client.*
 import io.ktor.client.request.*
+import io.ktor.http.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.koin.core.component.KoinComponent
-import org.koin.java.KoinJavaComponent.inject
 
 class Settings(private val settingsRepository: SettingsRepository) {
 
@@ -35,10 +34,10 @@ class Settings(private val settingsRepository: SettingsRepository) {
         CoroutineScope(Dispatchers.IO).launch {
             httpClient.put("${Const.connection.URL}/users/change_password") {
                 bearerAuth(token)
-                TODO()
+                contentType(ContentType.Application.Json)
+                setBody(ChangePasswordReceiveDTO(password))
             }
         }
-
     }
 
 
--- app/src/main/java/com/kursor/chroniclesofww2/di/ConnectionModule.kt
+++ app/src/main/java/com/kursor/chroniclesofww2/di/ConnectionModule.kt
@@ -1,10 +1,23 @@
 package com.kursor.chroniclesofww2.di
 
 import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.websocket.*
+import io.ktor.serialization.kotlinx.json.*
+
 import org.koin.dsl.module
 
 val connectionModule = module {
 
-    single { HttpClient() }
+    single {
+        HttpClient(CIO) {
+            install(WebSockets)
+
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+    }
 
 }
