a248b6ff2c450f59a59c3a35844ce67c801be8b4
kursor <skurochkin298@gmail.com>
2022-09-20 01:07:47 +0300

    new: change password feature

--- app/src/main/java/com/kursor/chroniclesofww2/presentation/ui/fragments/features/user/ChangePasswordFragment.kt
+++ app/src/main/java/com/kursor/chroniclesofww2/presentation/ui/fragments/features/user/ChangePasswordFragment.kt
@@ -1,9 +1,58 @@
 package com.kursor.chroniclesofww2.presentation.ui.fragments.features.user
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import com.kursor.chroniclesofww2.databinding.FragmentChangePasswordBinding
+import com.kursor.chroniclesofww2.viewModels.features.user.ChangePasswordViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ChangePasswordFragment : Fragment() {
 
+    lateinit var binding: FragmentChangePasswordBinding
 
+    val changePasswordViewModel by viewModel<ChangePasswordViewModel>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentChangePasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.newPasswordEditText.doOnTextChanged { text, start, before, count ->
+            changePasswordViewModel.setNewPassword(text.toString())
+        }
+
+        binding.currentPasswordEditText.doOnTextChanged { text, start, before, count ->
+            changePasswordViewModel.setCurrentPassword(text.toString())
+        }
+
+        binding.repeatPasswordEditText.doOnTextChanged { text, start, before, count ->
+            changePasswordViewModel.setRepeatPassword(text.toString())
+        }
+
+        binding.readyButton.setOnClickListener {
+            if (binding.newPasswordEditText.text != binding.repeatPasswordEditText.text) {
+                Toast.makeText(
+                    requireContext(),
+                    "Passwords don't match",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            changePasswordViewModel.ready()
+        }
+
+        changePasswordViewModel.statusLiveData.observe(viewLifecycleOwner) {
+            Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
+        }
+    }
 }
---
+++ app/src/main/java/com/kursor/chroniclesofww2/viewModels/features/user/ChangePasswordViewModel.kt
@@ -0,0 +1,51 @@
+package com.kursor.chroniclesofww2.viewModels.features.user
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kursor.chroniclesofww2.domain.useCases.user.ChangePasswordUseCase
+import com.kursor.chroniclesofww2.features.ChangePasswordReceiveDTO
+import com.kursor.chroniclesofww2.presentation.ui.fragments.features.user.ChangePasswordFragment
+import kotlinx.coroutines.launch
+
+class ChangePasswordViewModel(
+    val changePasswordUseCase: ChangePasswordUseCase
+) : ViewModel() {
+
+    private var currentPassword = ""
+    private var newPassword = ""
+    private var repeatPassword = ""
+
+    private val _statusLiveData = MutableLiveData<String>()
+    val statusLiveData: LiveData<String> get() = _statusLiveData
+
+    fun setCurrentPassword(password: String) {
+        currentPassword = password
+    }
+
+    fun setNewPassword(password: String) {
+        newPassword = password
+    }
+
+    fun setRepeatPassword(password: String) {
+        repeatPassword = password
+    }
+
+    fun ready() {
+        if (newPassword != repeatPassword) return
+        viewModelScope.launch {
+            changePasswordUseCase(
+                ChangePasswordReceiveDTO(
+                    currentPassword = currentPassword,
+                    newPassword = newPassword
+                )
+            ).onSuccess {
+                _statusLiveData.postValue(it.message)
+            }.onFailure {
+                _statusLiveData.postValue("Exception")
+            }
+        }
+    }
+
+}
