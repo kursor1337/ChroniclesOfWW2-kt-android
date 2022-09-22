5db09530ac5af4525052cbccb975ec91655320e7
kursor <skurochkin298@gmail.com>
2022-09-23 00:57:54 +0300

    making toolbar for publishing battles

--- data/src/main/java/com/kursor/chroniclesofww2/data/repositories/database/typeConverters/DivisionResourcesMapTypeConverter.kt
+++ data/src/main/java/com/kursor/chroniclesofww2/data/repositories/database/typeConverters/DivisionResourcesMapTypeConverter.kt
@@ -7,14 +7,18 @@ import com.squareup.moshi.Types
 
 class DivisionResourcesMapTypeConverter {
 
-    val type = Types.newParameterizedType(Map::class.java, Division.Type::class.java, Int::class.java)
+    val type = Types.newParameterizedType(
+        Map::class.java,
+        Division.Type::class.java,
+        Int::class.javaObjectType
+    )
 
     val adapter: JsonAdapter<Map<Division.Type, Int>> = moshi.adapter(type)
 
     @TypeConverter
-    fun fromMapToJson(map: Map<Division.Type, Int>) = adapter.toJson(map)
+    fun fromMapToJson(map: Map<Division.Type, Int>): String = adapter.toJson(map)
 
     @TypeConverter
-    fun fromJsonToMap(string: String) = adapter.fromJson(string)!!
+    fun fromJsonToMap(string: String): Map<Division.Type, Int> = adapter.fromJson(string)!!
 
 }
